784aceaa095369d308517f0ec6407030b7e8e2fe
Camelo-Albino <gabriel.marchi@uscsonline.com.br>
2025-10-07 17:40:28 -0300

    Testando BD

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,5 +1,6 @@
-using System.Diagnostics;
 using UnityEngine;
+using System.Linq;
+using System.Collections.Generic;
 
 public class GameManager : MonoBehaviour
 {
@@ -7,20 +8,24 @@ public class GameManager : MonoBehaviour
     void Start()
     {
         db = GetComponent<GameDatabase>();
-
         // Teste: salvar dados
-        db.SalvarConfiguracoes(0.7f, 0.9f, "1280x720", false);
-        db.SalvarProgresso(2, "Adaga", "", "");
+        //db.SalvarConfiguracoes(0.7f, 0.9f, "1280x720", false);
+        //db.SalvarProgresso(2, "Adaga", "Banana");
 
         // Teste: carregar dados
         var cfg = db.CarregarConfiguracoes();
         Debug.Log($"Config -> Música: {cfg.VolumeMusica}, Efeitos: {cfg.VolumeEfeitos}, Res: {cfg.Resolucao}, Tela Cheia: {cfg.TelaCheia}");
 
         var prog = db.CarregarProgresso();
+        List<string> result = prog.IngredientesPerdidos.Split(new char[] { ',' }).ToList();
+        foreach (var item in result)
+        {
+            Debug.Log("Oi");
+            Debug.Log(item);
+        }
+        //Testando Parse
         //Debug.Log($"Progresso -> Nível: {prog.NivelAtual}, Pontos: {prog.Pontos}, Itens: {prog.Itens}");
     }
-
-    // Update is called once per frame
     void Update()
     {
         
