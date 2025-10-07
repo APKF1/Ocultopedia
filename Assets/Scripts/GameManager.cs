dc3d86caea1d1b4ec5db077aa46fd1d4b155e874
Camelo-Albino <gabriel.marchi@uscsonline.com.br>
2025-10-07 17:56:32 -0300

    Esqueci de umas coisinhas do commit passado hihihi

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -9,8 +9,8 @@ public class GameManager : MonoBehaviour
     {
         db = GetComponent<GameDatabase>();
         // Teste: salvar dados
-        //db.SalvarConfiguracoes(0.7f, 0.9f, "1280x720", false);
-        //db.SalvarProgresso(2, "Adaga", "Banana");
+        db.SalvarConfiguracoes(0.7f, 0.9f, "1280x720", false);
+        db.SalvarProgresso(2, "Adaga, Espada", "Banana");
 
         // Teste: carregar dados
         var cfg = db.CarregarConfiguracoes();
@@ -20,7 +20,6 @@ public class GameManager : MonoBehaviour
         List<string> result = prog.IngredientesPerdidos.Split(new char[] { ',' }).ToList();
         foreach (var item in result)
         {
-            Debug.Log("Oi");
             Debug.Log(item);
         }
         //Testando Parse
