2ecf65ab1cbe25149413ec530b98bac26538f163
ANTONIO PEDRO KAPITANOVAS FERREIRA <antonio.ferreira1@uscsonline.com.br>
2026-01-19 20:30:18 -0300

    Clientes

--- Assets/Scripts/Fase Config/Clientes Config/CustomerManager.cs
+++ Assets/Scripts/Fase Config/Clientes Config/CustomerManager.cs
@@ -31,8 +31,53 @@ public class CustomerManager : MonoBehaviour
         switch (nomeCliente)
         {
             case "Cliente 1":
-                posX = -2.5f;
-                posY = -1.35f;
+                posX = -2.97f;
+                posY = -1.307f;
+                break;
+
+            case "Cliente 2":
+                posX = -3.208139f;
+                posY = -1.8f;
+                break;
+
+            case "Cliente 3":
+                posX = -3.21f;
+                posY = -1.13f;
+                break;
+
+            case "Cliente 4":
+                posX = -3.11f;
+                posY = -2.29f;
+                break;
+
+            case "Cliente 5":
+                posX = -2.38f;
+                posY = -1.92f;
+                break;
+
+            case "Cliente 6":
+                posX = -2.98f;
+                posY = -2.09f;
+                break;
+
+            case "Cliente 7":
+                posX = -2.51f;
+                posY = -1.7942f;
+                break;
+
+            case "Cliente 8":
+                posX = -3.45f;
+                posY = -0.62f;
+                break;
+
+            case "Cliente 9":
+                posX = -2.61f;
+                posY = -1.84f;
+                break;
+
+            case "Cliente 10":
+                posX = -3.8591f;
+                posY = -0.53f;
                 break;
 
             default:
