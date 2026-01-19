b39603a0f212cbc2c7caf325addbda40e4c46349
felipebassetto-debug <felipe.bassetto@uscsonline.com.br>
2026-01-19 20:29:29 -0300

    configs

---
+++ Assets/Scripts/Configurações/Configs.cs
@@ -0,0 +1,29 @@
+using TMPro;
+using UnityEngine;
+
+public class Configs : MonoBehaviour
+{
+    [SerializeField] private string[] arrResolution;
+    [SerializeField] private TextMeshPro res;
+    private int atualIndex;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void ResolutionNext()
+    {
+    }
+
+    void ResolutionBack()
+    {
+
+    }
+}
