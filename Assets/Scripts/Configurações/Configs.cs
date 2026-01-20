5e3b2d6d167c171b7040ecdd8b414eef6713fb4e
felipebassetto-debug <felipe.bassetto@uscsonline.com.br>
2026-01-20 17:48:56 -0300

    configurações tela cheia

--- Assets/Scripts/Configurações/Configs.cs
+++ Assets/Scripts/Configurações/Configs.cs
@@ -3,13 +3,27 @@ using UnityEngine;
 
 public class Configs : MonoBehaviour
 {
-    [SerializeField] private string[] arrResolution;
-    [SerializeField] private TextMeshPro res;
-    private int atualIndex;
+    public TMP_Text res;
+    public GameDatabase db;
+    float volMusic;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        var data = db.CarregarConfiguracoes();
+        if (data.TelaCheia == 1)
+        {
+            Screen.fullScreen = true;
+            res.text = "Sim";
+        }
+        else if (data.TelaCheia != 1)
+        {
+            Screen.fullScreen = false;
+            res.text = "Não";
+        }
+        else Debug.Log("Erro tela cheia");
+
+        volMusic = data.VolumeMusica;
     }
 
     // Update is called once per frame
@@ -20,10 +34,22 @@ public class Configs : MonoBehaviour
 
     void ResolutionNext()
     {
+        if (!Screen.fullScreen)
+        {
+            Screen.fullScreen = true;
+            res.text = "Sim";
+            db.SalvarConfiguracoes(volMusic, true);
+        }
     }
 
     void ResolutionBack()
     {
+        if (Screen.fullScreen)
+        {
+            Screen.fullScreen = false;
+            res.text = "Não";
+            db.SalvarConfiguracoes(volMusic, false);
+        }
 
     }
 }
