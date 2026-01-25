0c25466cf8d67ac74bbcc4ca63eb525c5e597ce0
felipebassetto-debug <felipe.bassetto@uscsonline.com.br>
2026-01-25 18:14:05 -0300

    ajustes banco e script de banco

--- Assets/Scripts/Banco de Dados/GameDb.cs
+++ Assets/Scripts/Banco de Dados/GameDb.cs
@@ -10,27 +10,34 @@ public class GameDatabase : MonoBehaviour
     void Awake()
     {
         string dbPath = Path.Combine(Application.persistentDataPath, "savegame.db");
+        if(!File.Exists(dbPath))
+        {
+            string origemDb = Application.dataPath + "/Banco/savegame.db";
+            string destinoDb = dbPath;
+            File.Copy(origemDb, destinoDb);
+        }
+
         db = new SQLiteConnection(dbPath, SQLiteOpenFlags.ReadWrite | SQLiteOpenFlags.Create);
         Debug.Log("Banco criado/carregado em: " + dbPath);
 
-        db.CreateTable<Configuracoes>();
-        db.CreateTable<Progresso>();
-        db.CreateTable<Colecao>();
+        //db.CreateTable<Configuracoes>();
+        //db.CreateTable<Progresso>();
+        //db.CreateTable<Colecao>();
 
         // Se ainda não existir dados, cria com valores padrão
-        if (db.Table<Configuracoes>().Count() == 0)
-        {
-            SalvarConfiguracoes(1f, true);
-        }
+        //if (db.Table<Configuracoes>().Count() == 0)
+        //{
+        //    SalvarConfiguracoes(1f, true);
+        //}
 
-        if (db.Table<Progresso>().Count() == 0)
-        {
-            db.Insert(new Progresso { NivelProgresso = 1, NivelAtual =  1 });
-        }
+        //if (db.Table<Progresso>().Count() == 0)
+        //{
+        //    db.Insert(new Progresso { NivelProgresso = 1, NivelAtual =  1 });
+        //}
     }
 
     // ---------------- CONFIGURAÇÕES ----------------
-    public void SalvarConfiguracoes(float volumeMusica, bool telaCheia)
+    public void SalvarConfiguracoes(float volumeMusica, bool telaCheia) // mudar para update
     {
         db.DeleteAll<Configuracoes>(); // garante só 1 linha
         db.Insert(new Configuracoes
@@ -57,28 +64,28 @@ public class GameDatabase : MonoBehaviour
         return db.Table<Progresso>().FirstOrDefault();
     }
 
-    public Colecao CarregarColecao(string desbloq, int id)
+    /*public Colecao CarregarColecao(string desbloq, int id)
     {
         if (desbloq == null) return db.Table<Colecao>().FirstOrDefault();
-        else return db.Table<Colecao>().Where(c => c.Desbloqueavel == desbloq && c.Id == id).FirstOrDefault();
-    }
+        else return db.Table<Colecao>().Where(c => c.Coletado == desbloq && c.Id == id).FirstOrDefault();
+    }*/
 
     public void SalvarColecao(int id, bool coletado, string colecionavel)
     {
         db.Execute("UPDATE Colecao SET Coletado = ? WHERE Desbloqueavel = '?' AND Id = ?", coletado ? 1: 0, colecionavel, id);
     }
 
-    public void InserirColecao(string desb)
+    /*public void InserirColecao(string desb)
     {
         db.Insert(new Colecao
         {
             Coletado = false ? 1:0,
             Desbloqueavel = desb
         });
-    }
+    }*/
 
-
-    void OnDestroy()
+    
+    void OnDestroy() //Passar para obj dontDestroy
     {
         db?.Close();
     }
@@ -93,19 +100,50 @@ public class Configuracoes
     public int TelaCheia { get; set; } // 0 ou 1
 }
 
-public class Progresso
+public class Fases
 {
     [PrimaryKey, AutoIncrement]
     public int Id { get; set; }
+    public int Cliente { get; set; } 
+    public int Colecionavel { get; set; } 
+    public int Artefato { get; set; } 
+}
 
-    public int NivelProgresso { get; set; }
-    public int NivelAtual { get; set; }
+public class Clientes
+{
+    [PrimaryKey, AutoIncrement]
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public float PosX { get; set; }
+    public float PosY { get; set; }
 }
 
 public class Colecao
 {
     [PrimaryKey, AutoIncrement]
     public int Id { get; set; }
+    public string Name { get; set; }
+    public float PosX { get; set; }
+    public float PosY { get; set; }
     public int Coletado { get; set; } // 0 ou 1
-    public string Desbloqueavel { get; set; }
 }
+
+public class Progresso
+{
+    [PrimaryKey, AutoIncrement]
+    public int Id { get; set; }
+
+    public int Fase { get; set; }
+}
+
+public class Receitas
+{
+    [PrimaryKey, AutoIncrement]
+    public int Id { get; set; }
+    public string Item { get; set; }
+    public string Comp { get; set; }
+    public string Comp_2 { get; set; }
+    public string Comp_3 { get; set; }
+}
+
+
